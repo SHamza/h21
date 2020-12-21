5536942bd77e96ce0fcbc552bc656db4a5e20f31
SHamza <75013418+SHamza@users.noreply.github.com>
2020-12-21 23:48:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var wall,thickness;
+var bullet,speed,wheight;
+
+
+function setup() {
+  createCanvas(800,400);
+  thickness=random(22,83);
+  wall= createSprite(1200,200,thickness,height/2);
+  speed=random(223,321);
+  wheight=random(30,52);
+  damage=0.5.xbulletwheight
+}
+
+function draw() {
+  thickness=random(22,83);
+  wall= createSprite(1200,200,thickness,height/2);
+  speed=random(223,321);
+  wheight=random(30,52);
+  
+  drawSprites();
+}
